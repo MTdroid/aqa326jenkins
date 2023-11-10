6f90806547052fb1827591a4bca14e996f6dda08
”Marat <marattagirov2020@gmail.com>
2023-11-11 02:32:49 +0300

    a2

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -14,6 +14,19 @@ public class BoundaryServiceTest {
 
     BoundaryService boundaryService = new BoundaryService();
 
+    @ParameterizedTest
+    @DisplayName("Find min element in array")
+    @Description("testResult must be equals bsResult")
+    @MethodSource("arrays")
+    public void findmin(int[] arr) {
+        int bsResult = boundaryService.findMin(arr);
+        int testResult = Arrays.stream(arr).min().getAsInt();
+        assertEquals(testResult, bsResult);
+    }
+
+    static Stream<int[]> arrays() {
+        return Stream.of(new int[]{3, 2, 1, 6, 5, 1, 4, 4}, new int[]{0, 1, 0, 0, 2, 2});
+    }
 }
 
 
